cd1649108bb27a9f77f072fbf4618f703f846ea6
wnsvy2885@gmail.com <wnsvy2885@gmail.com>
2026-05-25 16:04:51 +0900

    Initial commit

---
+++ src/main/java/com/trip/routemate/RoutemateApplication.java
@@ -0,0 +1,13 @@
+package com.trip.routemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trip/routemate/RoutemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.routemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
